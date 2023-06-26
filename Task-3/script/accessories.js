14cc971925861d96674a544559b456f0c7d40e97
veeruadina <108525608+veeruadina@users.noreply.github.com>
2023-06-26 22:15:41 +0530

    Add files via upload

---
+++ Task-3/script/accessories.js
@@ -0,0 +1,53 @@
+import navbar from "./NAVBAR.js"
+document.getElementById("navbar").innerHTML=navbar()
+
+import {sidebar} from "./sidebar.js"
+let sidebar_div = document.getElementById("sidebar");
+sidebar_div.innerHTML = sidebar();
+
+import {top_bar} from "./top_bar.js"
+let top_barDiv=document.getElementById("dpdown")
+top_barDiv.innerHTML=top_bar()
+
+import { footer } from "./footer_product_page.js";
+document.getElementById("footer").innerHTML=footer();
+
+let container=document.getElementById("box")
+const mens_data= async()=>{
+    try{
+        let res=await fetch("https://script.google.com/macros/s/AKfycbx66wHklnOYjjDZn4bLnQiBnBZM6fgV8Gu66AtShDNjaKtMxfmG9vu1Hsj9D__Bs1Lk/exec")
+        let data= await res.json()
+        // let actual_data=await data
+        console.log(data.data)
+        appendData(data.data)
+
+}
+catch(error){
+console.log(error)
+}
+}
+mens_data()
+
+
+const appendData=(data)=>{
+
+    data.forEach(function(el){
+    //  console.log(el)
+        let div=document.createElement("div")
+        let result=document.getElementById("result_base")
+        let img=document.createElement("img")
+        img.src=el.image
+        console.log(img)
+        let title1=document.createElement("h3")
+        title1.innerHTML=el.brand
+         let title=document.createElement("p")
+         title.innerHTML=el.name
+
+         let cost=document.createElement("p")
+         cost.innerHTML="Rs. "+el.price
+
+         div.append(img,title1,title,cost)
+         result.append(div)
+         container.append(result)
+    })
+}
---
+++ Task-3/script/index.js
@@ -0,0 +1,19 @@
+import {navbar} from "./NAVBAR.js";
+document.querySelector(".container").innerHTML=navbar();
+
+
+import { footer } from "./footer.js";
+document.getElementById("footer").innerHTML=footer();
+document.querySelector("#goToBag").addEventListener("click", goToBag);
+
+function goToBag() {
+    console.log("hello")
+    window.location.href ="./html/cart.html";
+  }
+
+  document.querySelector(".profile").addEventListener('click',goToSignup)
+
+  function goToSignup(){
+    window.location.href="./html/signup.html"
+  }
+  
